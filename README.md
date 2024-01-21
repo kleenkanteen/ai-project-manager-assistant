6ad711aa40d24359d3dfe26cc5f96075db3ac882
kleenkanteen <antpalmolive@gmail.com>
2024-01-20 22:10:39 -0500

    got basic endpoint working so zapier can send every new transcript to /summary and then it is printed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 API server for the project manager assistant slack bot.
+
+First install the dependencies with `pip install -r requirements.txt`. Not using virtual environment.
+
+You also have to intall the ngrok cli. ngrok allows you to expose a web server running on your local machine to the internet. Just download the executable from [here](https://ngrok.com/download) and run it with `./ngrok http 8000`. This will expose the server running on port 8000 to the internet. You will get a url that you can use to send requests to your local server.
+
+Then run the server with `server.py`. The server will run on port 8000 by default.
--- main.py
+++ main.py
@@ -1,39 +1,67 @@
-from typing import Union
-from fastapi import FastAPI
-
 import os
-from slack_sdk import WebClient 
-from slack_sdk.errors import SlackApiError 
+import sys
+from typing import Union, List
+
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.logger import logger
+from fastapi.responses import JSONResponse
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 """ We need to pass the 'Bot User OAuth Token' """
-#slack_token = os.environ.get('SLACK_BOT_TOKEN')
-slack_token="xoxb-6505923377716-6497481574726-g1uu7VHP0qwwHgXL68O15GsF"
+# slack_token = os.environ.get('SLACK_BOT_TOKEN')
+slack_token = "do not commit token"
 print(f"{slack_token}")
 # Creating an instance of the Webclient class
 client = WebClient(token=slack_token)
 
-try:
-	# Posting a message in #random channel
-	response = client.chat_postMessage(
-    				channel="random",
-    				text="Testing")
-	print('Done 1')
-	# Sending a message to a particular user
-	response = client.chat_postEphemeral(
-                    channel="random", 
-                    text="Hello U06ETJNQX6E", 
-                    user="U06ETJNQX6E")
-	print('Done 2')
-	# Get basic information of the channel where our Bot has access 
-	response = client.conversations_info(
-                    channel="random")
-	print('Done 3')
-	# Get a list of conversations
-	response = client.conversations_list()
-	print(response["channels"])
-	
-except SlackApiError as e:
-	assert e.response["error"]
+BASE_URL = "http://localhost:8000"
+USE_NGROK = "True"
+
+def init_webhooks(base_url):
+    # Update inbound traffic via APIs to use the public-facing ngrok URL
+    pass
+
+
+if USE_NGROK:
+    # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
+    from pyngrok import ngrok
+
+    # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
+    # when starting the server
+    port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else "8000"
+
+    # Open a ngrok tunnel to the dev server
+    # I commented this part out because you should run "ngrok http 8000" in a separate terminal
+    # public_url = ngrok.connect(port).public_url
+    # print("ngrok url:", public_url)
+    # logger.info('ngrok tunnel "{}" -> "http://127.0.0.1:{}"'.format(public_url, port))
+
+    # Update any base URLs or webhooks to use the public ngrok URL
+    # commented out for now
+    # BASE_URL = public_url
+    # init_webhooks(public_url)
+
+
+# try:
+    # Posting a message in #random channel
+    # response = client.chat_postMessage(channel="random", text="Testing")
+    # print("Done 1")
+    # Sending a message to a particular user
+    # response = client.chat_postEphemeral(
+    #     channel="random", text="Hello U06ETJNQX6E", user="U06ETJNQX6E"
+    # )
+    # print("Done 2")
+    # Get basic information of the channel where our Bot has access
+    # response = client.conversations_info(channel="random")
+    # print("Done 3")
+    # Get a list of conversations
+    # response = client.conversations_list()
+    # print(response["channels"])
+
+# except SlackApiError as e:
+#     assert e.response["error"]
+#     print("slack bot error")
 
 app = FastAPI()
 
@@ -41,7 +69,21 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# @app.post("/summary")
+# def summary(request: Request):
+#     body = request.body()
+#     print("body", body)
+#     # body = request.json()
+#     # print("body", body)
+#     # return JSONResponse(content=body, status_code=200)
+#     return JSONResponse(content=body, status_code=200)
+
+@app.post("/summary")
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    print(contents.decode('utf-8'))
+    return JSONResponse(content={"message": "File contents printed successfully"}, status_code=200)
